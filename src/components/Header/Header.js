9e5b107842146da3fd43159df1ac14e2c801967c
VirtualVasquez <mxv647@med.miami.edu>
2018-12-18 20:36:06 -0500

    Fundamentals re-established

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Header.css";
+
+const Header = props => (
+	<header>
+    <p>Clicky Game</p>
+    <p>Click an image to begin!</p>
+    <p>Score: 0 | Top Score: 0</p>
+	</header>
+);
+
+export default Header;
