34f07f8c7be08a430623acfbc7b9a5e6eb4f49bf
VirtualVasquez <mxv647@med.miami.edu>
2018-12-22 10:28:03 -0500

    updating

---
+++ src/components/ClickItem/ClickItem.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react"
+import './ClickItem.css'
+
+const ClickItem = props => (
+	<div className="clickItem">
+		<div className="img-container">
+		</div>
+	</div>
+)
+
+export default ClickItem
