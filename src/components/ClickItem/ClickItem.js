6c2ea832af1bf8f788f97185c4b34886604af9cc
VirtualVasquez <mxv647@med.miami.edu>
2018-12-26 14:04:24 -0500

    added shake and color animation, rework page CSS and Readme

--- src/components/ClickItem/ClickItem.js
+++ src/components/ClickItem/ClickItem.js
@@ -4,7 +4,7 @@ import './ClickItem.css'
 const ClickItem = props => (
 	<div className="clickItem">
 		<div className="img-container">
-		<img onClick={() => props.selectCharacter(props.reploid)}  alt={props.reploid} src={props.image} />
+		<img className={props.curScore === 0 ? "style_prevu_kit_ex" : undefined} onClick={() => props.selectCharacter(props.reploid)} alt={props.reploid} src={props.image} />
 		</div>
 	</div>
 )
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,7 @@ import './Header.css'
 const Header = props => (
     <header>
     <h3>Clicky Game</h3>
-    <h3>{props.message}</h3>
+    <h3 className={props.message.indexOf('incorrectly') !== -1 ? "desc-incorrect" : props.message.indexOf('correctly') !== -1 ? "desc-correct" : "desc-normal"} >{props.message}</h3>
     <h3>Score: {props.curScore} | Top Score: {props.topScore}</h3>
     </header>
 )
