9b3b0f6a7fee0b85d3fe471a07c103554d1fb6e7
VirtualVasquez <mxv647@med.miami.edu>
2018-12-26 21:29:16 -0500

    Updates

---
+++ precache-manifest.eb436229a9277b3f9f80bc263bd86fe2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1e41d16bcd11f95a6fcb3e5d6c66bb31",
+    "url": "/click-game/static/media/mmxp2.1e41d16b.png"
+  },
+  {
+    "revision": "451cb8796073a9e8640b",
+    "url": "/click-game/static/js/runtime~main.451cb879.js"
+  },
+  {
+    "revision": "9c3234ccb449fd73cb0c",
+    "url": "/click-game/static/js/main.9c3234cc.chunk.js"
+  },
+  {
+    "revision": "860424fade0ab31f41bf",
+    "url": "/click-game/static/js/1.860424fa.chunk.js"
+  },
+  {
+    "revision": "9c3234ccb449fd73cb0c",
+    "url": "/click-game/static/css/main.17bcf8cc.chunk.css"
+  },
+  {
+    "revision": "b503375cfc6c8bbf9d0ca7cbe7903199",
+    "url": "/click-game/index.html"
+  }
+];
